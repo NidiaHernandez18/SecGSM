4c5b7924e82b0c609977896318390c0cddef78ed
NidiaHernandez18 <nidiaeloisa18@gmail.com>
2018-08-09 19:14:05 -0500

    segGSM15

--- SecGSM/frmlogin.Designer.vb
+++ SecGSM/frmlogin.Designer.vb
@@ -46,7 +46,7 @@ Partial Class frmlogin
         'label5
         '
         Me.label5.AutoSize = True
-        Me.label5.Location = New System.Drawing.Point(238, 361)
+        Me.label5.Location = New System.Drawing.Point(146, 229)
         Me.label5.Name = "label5"
         Me.label5.Size = New System.Drawing.Size(187, 13)
         Me.label5.TabIndex = 21
@@ -54,18 +54,18 @@ Partial Class frmlogin
         '
         'bttn_Cancelar
         '
-        Me.bttn_Cancelar.Location = New System.Drawing.Point(348, 287)
+        Me.bttn_Cancelar.Location = New System.Drawing.Point(266, 160)
         Me.bttn_Cancelar.Name = "bttn_Cancelar"
-        Me.bttn_Cancelar.Size = New System.Drawing.Size(93, 39)
+        Me.bttn_Cancelar.Size = New System.Drawing.Size(78, 30)
         Me.bttn_Cancelar.TabIndex = 20
         Me.bttn_Cancelar.Text = "Cancelar"
         Me.bttn_Cancelar.UseVisualStyleBackColor = True
         '
         'bttn_OK
         '
-        Me.bttn_OK.Location = New System.Drawing.Point(191, 287)
+        Me.bttn_OK.Location = New System.Drawing.Point(134, 160)
         Me.bttn_OK.Name = "bttn_OK"
-        Me.bttn_OK.Size = New System.Drawing.Size(93, 39)
+        Me.bttn_OK.Size = New System.Drawing.Size(78, 29)
         Me.bttn_OK.TabIndex = 19
         Me.bttn_OK.Text = "OK"
         Me.bttn_OK.UseVisualStyleBackColor = True
@@ -74,9 +74,9 @@ Partial Class frmlogin
         '
         Me.pictureBox1.BackgroundImage = CType(resources.GetObject("pictureBox1.BackgroundImage"), System.Drawing.Image)
         Me.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
-        Me.pictureBox1.Location = New System.Drawing.Point(533, 177)
+        Me.pictureBox1.Location = New System.Drawing.Point(385, 103)
         Me.pictureBox1.Name = "pictureBox1"
-        Me.pictureBox1.Size = New System.Drawing.Size(63, 66)
+        Me.pictureBox1.Size = New System.Drawing.Size(46, 51)
         Me.pictureBox1.TabIndex = 10
         Me.pictureBox1.TabStop = False
         '
@@ -85,9 +85,9 @@ Partial Class frmlogin
         Me.pictureBox2.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
         Me.pictureBox2.BackgroundImage = CType(resources.GetObject("pictureBox2.BackgroundImage"), System.Drawing.Image)
         Me.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.pictureBox2.Location = New System.Drawing.Point(43, 305)
+        Me.pictureBox2.Location = New System.Drawing.Point(13, 182)
         Me.pictureBox2.Name = "pictureBox2"
-        Me.pictureBox2.Size = New System.Drawing.Size(81, 78)
+        Me.pictureBox2.Size = New System.Drawing.Size(66, 58)
         Me.pictureBox2.TabIndex = 12
         Me.pictureBox2.TabStop = False
         '
@@ -98,7 +98,7 @@ Partial Class frmlogin
         Me.panel1.Controls.Add(Me.pictureBox3)
         Me.panel1.Location = New System.Drawing.Point(0, 0)
         Me.panel1.Name = "panel1"
-        Me.panel1.Size = New System.Drawing.Size(633, 103)
+        Me.panel1.Size = New System.Drawing.Size(463, 66)
         Me.panel1.TabIndex = 18
         '
         'label4
@@ -106,7 +106,7 @@ Partial Class frmlogin
         Me.label4.AutoSize = True
         Me.label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight
-        Me.label4.Location = New System.Drawing.Point(161, 43)
+        Me.label4.Location = New System.Drawing.Point(127, 24)
         Me.label4.Name = "label4"
         Me.label4.Size = New System.Drawing.Size(0, 24)
         Me.label4.TabIndex = 8
@@ -116,16 +116,16 @@ Partial Class frmlogin
         '
         Me.pictureBox3.BackgroundImage = CType(resources.GetObject("pictureBox3.BackgroundImage"), System.Drawing.Image)
         Me.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
-        Me.pictureBox3.Location = New System.Drawing.Point(43, 3)
+        Me.pictureBox3.Location = New System.Drawing.Point(27, 8)
         Me.pictureBox3.Name = "pictureBox3"
-        Me.pictureBox3.Size = New System.Drawing.Size(86, 97)
+        Me.pictureBox3.Size = New System.Drawing.Size(61, 53)
         Me.pictureBox3.TabIndex = 7
         Me.pictureBox3.TabStop = False
         '
         'label3
         '
         Me.label3.AutoSize = True
-        Me.label3.Location = New System.Drawing.Point(90, 244)
+        Me.label3.Location = New System.Drawing.Point(19, 137)
         Me.label3.Name = "label3"
         Me.label3.Size = New System.Drawing.Size(61, 13)
         Me.label3.TabIndex = 17
@@ -134,7 +134,7 @@ Partial Class frmlogin
         'label2
         '
         Me.label2.AutoSize = True
-        Me.label2.Location = New System.Drawing.Point(90, 199)
+        Me.label2.Location = New System.Drawing.Point(19, 111)
         Me.label2.Name = "label2"
         Me.label2.Size = New System.Drawing.Size(43, 13)
         Me.label2.TabIndex = 16
@@ -143,7 +143,7 @@ Partial Class frmlogin
         'label1
         '
         Me.label1.AutoSize = True
-        Me.label1.Location = New System.Drawing.Point(90, 149)
+        Me.label1.Location = New System.Drawing.Point(19, 76)
         Me.label1.Name = "label1"
         Me.label1.Size = New System.Drawing.Size(60, 26)
         Me.label1.TabIndex = 15
@@ -151,32 +151,31 @@ Partial Class frmlogin
         '
         'txt_password
         '
-        Me.txt_password.Location = New System.Drawing.Point(186, 244)
+        Me.txt_password.Location = New System.Drawing.Point(115, 134)
         Me.txt_password.Name = "txt_password"
-        Me.txt_password.Size = New System.Drawing.Size(287, 20)
+        Me.txt_password.Size = New System.Drawing.Size(255, 20)
         Me.txt_password.TabIndex = 14
         Me.txt_password.UseSystemPasswordChar = True
         '
         'txt_Usuario
         '
-        Me.txt_Usuario.Location = New System.Drawing.Point(186, 199)
+        Me.txt_Usuario.Location = New System.Drawing.Point(115, 108)
         Me.txt_Usuario.Name = "txt_Usuario"
-        Me.txt_Usuario.Size = New System.Drawing.Size(287, 20)
+        Me.txt_Usuario.Size = New System.Drawing.Size(255, 20)
         Me.txt_Usuario.TabIndex = 13
         '
         'comB_Empresas
         '
         Me.comB_Empresas.FormattingEnabled = True
-        Me.comB_Empresas.Location = New System.Drawing.Point(186, 154)
+        Me.comB_Empresas.Location = New System.Drawing.Point(115, 81)
         Me.comB_Empresas.Name = "comB_Empresas"
-        Me.comB_Empresas.Size = New System.Drawing.Size(288, 21)
+        Me.comB_Empresas.Size = New System.Drawing.Size(255, 21)
         Me.comB_Empresas.TabIndex = 11
         '
         'frmlogin
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(632, 395)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit
+        Me.ClientSize = New System.Drawing.Size(453, 252)
         Me.Controls.Add(Me.label5)
         Me.Controls.Add(Me.bttn_Cancelar)
         Me.Controls.Add(Me.bttn_OK)
